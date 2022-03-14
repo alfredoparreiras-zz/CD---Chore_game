09b88a2949872e4de2401dd70192234dfe734744
alfredo <a.alfredops@gmail.com>
2022-03-14 09:23:06 -0300

    update

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
 
+let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg'
+
 // Amount of Doors in the Game 
 let numClosedDoors = 3; 
 
 // Doors Opened 
 let openDoor1 = null;
-let openDoor2 = 'segundo teste';
-let openDoor3 = 'teste';
+let openDoor2 = null;
+let openDoor3 = null;
 
 // New path contain the opened door
 const botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
@@ -44,18 +46,40 @@ const randomDoorGenerator = () => {
     };
 };
 
-randomDoorGenerator(); 
+// Function to require that all ports are clicked
+const isClicked = (door) => { 
+
+}
+
+//Function to determine Victory
+function playDoor() { 
+    numClosedDoors = numClosedDoors - 1;
+
+    if(numClosedDoors === 0 ){ 
+        gameOver()
+    }
+
+    return numClosedDoors;
+}
+
+
 
 // Functions to open the doors. 
 doorImage1.onclick = () => { 
     doorImage1.src = openDoor1;
+    playDoor();
 };
 
 doorImage2.onclick = () => { 
     doorImage2.src = openDoor2;
+    playDoor();
 };
 
 doorImage3.onclick = () => { 
     doorImage3.src = openDoor3;
+    playDoor();
 };
 
+
+// Calling the Generator Function 
+randomDoorGenerator(); 
